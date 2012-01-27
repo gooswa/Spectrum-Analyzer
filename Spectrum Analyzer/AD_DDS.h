8efdc77bde90204d967d9fd8fde4c4b48a8fc743
William Dillon <hpux735@gmail.com>
2012-01-26 18:10:36 -0800

    Added the DDS class (seems to work) to git

---
+++ Spectrum Analyzer/AD_DDS.h
@@ -0,0 +1,41 @@
+//
+//  AD_DDS.h
+//  Spectrum Analyzer
+//
+//  Created by William Dillon on 1/22/12.
+//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class HardwareInterface;
+
+@interface AD_DDS : NSObject
+{
+    // Pin numbers for use with the hardware interface
+    int CS, Data, Clock;
+    
+    // Frequencies
+    double refFreq;
+    double phase;
+    double outputFreq;
+    
+    // Chip options
+    bool powerDown;
+    
+    // Delta phase value
+    uint32 deltaPhase;
+}
+
+@property(readwrite) int CS;
+@property(readwrite) int Data;
+@property(readwrite) int Clock;
+@property(readwrite) double refFreq;
+@property(readwrite) double phase;
+@property(readwrite) double outputFreq;
+@property(readwrite) bool powerDown;
+
+- (void)initHardware:(HardwareInterface *)interface;
+- (void)updateHardware:(HardwareInterface *)interface;
+
+@end
---
+++ Spectrum Analyzer/AD_DDS.m
@@ -0,0 +1,120 @@
+//
+//  AD_DDS.m
+//  Spectrum Analyzer
+//
+//  Created by William Dillon on 1/22/12.
+//  Copyright (c) 2012 Oregon State University (COAS). All rights reserved.
+//
+
+#import "AD_DDS.h"
+#import "HardwareInterface.h"
+
+@implementation AD_DDS
+
+@synthesize CS;
+@synthesize Data;
+@synthesize Clock;
+@synthesize refFreq;
+@synthesize phase;
+@synthesize outputFreq;
+@synthesize powerDown;
+
+static uint8 reverse[16] = {
+    0x0, // 0000 -> 0000
+    0x8, // 0001 -> 1000
+    0x4, // 0010 -> 0100
+    0xC, // 0011 -> 1100
+    0x2, // 0100 -> 0010
+    0xA, // 0101 -> 1010
+    0x6, // 0110 -> 0110
+    0xE, // 0111 -> 1110
+    0x1, // 1000 -> 0001
+    0x9, // 1001 -> 1001
+    0x5, // 1010 -> 0101
+    0xD, // 1011 -> 1101
+    0x3, // 1100 -> 0011
+    0xB, // 1101 -> 1011
+    0x7, // 1110 -> 0111
+    0xF, // 1111 -> 1111
+};
+
+#define RESERVED_WORD_MASK 0xFC
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        // Set the defaults
+        outputFreq = 10.7;
+    }
+    
+    return self;
+}
+
+-(void)initHardware:(HardwareInterface *)interface
+{
+    // Clock the parallel word once (loads serial mode into config)
+    [interface setPin:Clock Value:0];
+    [interface setPin:Clock Value:1];
+    [interface setPin:Clock Value:0];
+    
+    // Clock the latch
+    [interface setPin:CS Value:0];
+    [interface setPin:CS Value:1];
+    [interface setPin:CS Value:0];
+    
+    // Now, the device is in serial mode, ready for loading
+    // If, for some reason, the device was already in serial mode,
+    // or is otherwise FUBAR'd, try to clear out the registers by
+    // loading all 0's...
+    [interface sendSPIwithPin:Data
+                        Clock:Clock
+                           CS:CS
+                         data:0
+                         bits:40];
+}
+
+// This is applied to the phase/control word (bits 33 through 40)
+
+- (void)updateHardware:(HardwareInterface *)interface
+{
+    // Calculate the delta phase value
+    // This is the equation from the data sheet, but
+    // re-arranged to solve for delta phase rather than Fout
+    double floatPhase = (outputFreq * exp2(32)) / refFreq;
+    if (floatPhase > UINT32_MAX) {
+        NSLog(@"The calculated delta phase is greater than uint32");
+    }
+    
+    deltaPhase = (uint32)round(floatPhase);
+    
+    // Re-calculate the output frequency from the delta phase
+    // This is the same equation that's in the datasheet
+    outputFreq = ((double)deltaPhase * refFreq) / exp2(32);
+    
+    // If we have a hardware interface, update the device
+    if (interface) {
+        uint64 reg = deltaPhase;
+        
+        // Because the DDS is loaded LSB-first,
+        // we need to convert the register here.
+        // To do this, we'll reverse each nibble,
+        // then load them into a new register backwards.
+        uint64 reg2 = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            reg2  = reg2 << 4;
+            reg2 |= reverse[(reg >> i*4) & 0xf];
+        }
+        
+        
+        [interface sendSPIwithPin:Data
+                            Clock:Clock
+                               CS:CS
+                             data:reg2
+                             bits:40];
+    }
+}
+
+@end
