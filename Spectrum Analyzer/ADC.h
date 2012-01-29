de91fa4e5f99fa13443cceb6e0092d56c2558f90
William Dillon <hpux735@gmail.com>
2012-01-29 10:49:53 -0800

    Added an ADC class

---
+++ Spectrum Analyzer/ADC.h
@@ -0,0 +1,13 @@
+//
+//  ADC.h
+//  Spectrum Analyzer
+//
+//  Created by William Dillon on 1/29/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADC : NSObject
+
+@end
---
+++ Spectrum Analyzer/ADC.m
@@ -0,0 +1,13 @@
+//
+//  ADC.m
+//  Spectrum Analyzer
+//
+//  Created by William Dillon on 1/29/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import "ADC.h"
+
+@implementation ADC
+
+@end
