f6aef4f882ae3f7b42951633eaad3605f4d7e71d
William Dillon <hpux735@gmail.com>
2012-02-03 17:01:45 -0800

    Added a delegation protocol for the hardware modules

---
+++ Spectrum Analyzer/ModuleDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ModuleDelegate.h
+//  Spectrum Analyzer
+//
+//  Created by William Dillon on 2/3/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ModuleDelegate <NSObject>
+
+@optional
+- (void)moduleDidBecomeStale;
+- (void)moduleWillUpdateHardware;
+
+@end
