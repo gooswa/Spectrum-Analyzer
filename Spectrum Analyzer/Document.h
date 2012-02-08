8ce4d1649386db261e4b48ab94c1dbc9dd8649b7
William Dillon <hpux735@gmail.com>
2012-02-07 20:42:47 -0800

    Added files for an acceptable spectrum analyzer application

---
+++ Spectrum Analyzer/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Spectrum Analyzer
+//
+//  Created by William Dillon on 2/6/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
